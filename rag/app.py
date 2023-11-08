24f0f45416ffba3ffb84c9d9aebc086bd5e7b36b
Brandon <brandon@wizardondemand.com>
2023-11-08 12:18:37 -0500

    somehow the formatter misspelled documentation

--- rag/app.py
+++ rag/app.py
@@ -6,7 +6,7 @@ import chains
 
 
 # Initialize the application chain
-chain = chains.DocumantationChain("https://docs.smith.langchain.com").get_chain()
+chain = chains.DocumentationChain("https://docs.smith.langchain.com").get_chain()
 
 
 app = Flask(__name__)
